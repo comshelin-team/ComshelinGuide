004c6f78a8f57ea65583456f1e0afaacf0023441
devleejb <dev.leejb@gmail.com>
2019-08-12 13:13:41 +0900

    Error Occured

---
+++ AndroidStudioProjects/ComshelinGuide/app/src/main/java/com/devljb/comshelinguide/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.devljb.comshelinguide
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.comshelin.comshelinguide.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
