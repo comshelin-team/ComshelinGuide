549bd3f649e40456cac1c492a53ee3042e6f34c3
devleejb <dev.leejb@gmail.com>
2019-11-12 14:02:54 +0900

    Delete All

--- AndroidStudioProjects/ComshelinGuide/app/src/main/java/com/comshelin/comshelinguide/Restaurant.kt
+++
@@ -1,6 +0,0 @@
-package com.comshelin.comshelinguide
-
-import android.graphics.Bitmap
-import java.io.Serializable
-
-data class Restaurant(val name: String, val location: String, val comment: String, val image: ByteArray) : Serializable
